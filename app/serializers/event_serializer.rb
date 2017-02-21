141c0c9697cf2a2b5b9ed8fbe08ee28a610bd4e4
almikola <almikola@gmail.com>
2017-02-21 12:33:11 +0000

    events now showing for user

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :title, :date, :time, :owner_id
+  belongs_to :owner, foreign_key: "owner_id", class_name: "User"
+  has_many :invitations
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email
+  has_many :events, foreign_key: :owner_id, class_name: "User"
+  has_many :received_invitations, foreign_key: :receiver_id, class_name: "Invitation"
 end
